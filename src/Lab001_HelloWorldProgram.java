2e494c680c533451801408d2f3ea3358295138a5
aksha <akshaygodase108@gmail.com>
2025-10-30 18:13:02 +0530

    i am pushing the Code

--- src/Lab001_HelloWorldProgram.java
+++ src/Lab001_HelloWorldProgram.java
@@ -1,5 +1,6 @@
 public class Lab001_HelloWorldProgram {
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("HelloWorldProgram");
     }
 }
