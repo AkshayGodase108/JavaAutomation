574ae9ea6d882dd563c7199071346a9569b962f5
aksha <akshaygodase108@gmail.com>
2025-12-06 18:10:26 +0530

    Java programs

--- src/exc27_Generic/Lab178_Generics_List.java
+++ src/exc27_Generic/Lab178_Generics_List.java
@@ -4,16 +4,12 @@ import java.util.List;
 public class Lab178_Generics_List {
 
 
-
-
-
-
         public static void main(String[] args) {
             List int0 = new ArrayList(); // Generic , INT, STR, DOUBLE, FLOAT...
             List<Integer> int1 = new ArrayList();
             List<String> int2 = new ArrayList();
             List<Double> int3 = new ArrayList();
-            List<Boolean> int4 = new ArrayList();
+            List<Boolean> int4 = new ArrayList(); //
         }
 
 
