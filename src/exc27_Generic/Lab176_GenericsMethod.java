5181f696617eb789e4166be74ede3a8231efd989
aksha <akshaygodase108@gmail.com>
2025-12-06 18:09:33 +0530

    Java programs

---
+++ src/exc27_Generic/Lab176_GenericsMethod.java
@@ -0,0 +1,20 @@
+package exc27_Generic;
+
+public class Lab176_GenericsMethod {
+
+        public static void main(String[] args) {
+
+            temp_sum(3,4);
+            temp_sum(3.34,4.45);
+            temp_sum("Pramod","Dutta");
+
+
+        }
+
+        static <G> G temp_sum(G a, G b){
+            System.out.println(a);
+            System.out.println(b);
+            return null;
+        }
+
+    }
---
+++ src/exc27_Generic/Lab177_Generic_Class.java
@@ -0,0 +1,25 @@
+package exc27_Generic;
+
+public class Lab177_Generic_Class {
+
+        public static void main(String[] args) {
+            GenericClass<Integer> intObjRef = new GenericClass(5);
+            GenericClass<String> stringObjRef = new GenericClass("Pramod");
+
+            // T -> it is placeholder only -> it can be any data type.
+
+        }
+    }
+
+    class GenericClass<T>{
+        private T data;
+
+        public GenericClass(T data){
+            this.data = data;
+        }
+
+        public T getData(){
+            return this.data;
+        }
+
+    }
